f2156b51da646740f1526df7f5a0aeda15237be6
Guilherme Nunes <guiicnunes@gmail.com>
2021-09-06 21:14:24 -0300

    FEAT: Quem sou eu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from './components/Header';
 import './App.css';
 import Hero from './components/Hero';
+import WhoIAm from './components/WhoIAm';
 
 function App() {
   return (
     <div>
       <Header />
       <Hero />
+      <WhoIAm />
     </div>
   );
 }
---
+++ src/components/WhoIAm.js
@@ -0,0 +1,24 @@
+import '../style/whoIAm.css';
+
+export default function WhoIAm() {
+  return (
+    <section className="who-i-am">
+      <article className="who-i-am-content">
+        <section className="who-i-am-content-about">
+          <img alt="Foto de Guilherme Nunes" src="./who-i-am-image.jpeg" className="who-i-am-content-image"/>
+          <section className="who-i-am-text">
+            <p>
+              Mussum Ipsum, cacilds vidis litro abertis. Atirei o pau no gatis, per gatis num morreus. Praesent vel viverra nisi. Mauris aliquet nunc non turpis scelerisque, eget. Nullam volutpat risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio. Cevadis im ampola pa arma uma pindureta.
+            </p>
+            <p>
+              Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis! Per aumento de cachacis, eu reclamis. Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose. Vehicula non. Ut sed ex eros. Vivamus sit amet nibh non tellus tristique interdum.
+            </p>
+          </section>
+        </section>
+        <section className="who-i-am-timeline">
+          Aqui vai a time line
+        </section>
+      </article>
+    </section>
+  );
+}
