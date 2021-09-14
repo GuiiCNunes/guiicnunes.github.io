72ea3ec1f30cb30fb91deab3b83ff5e156d0ace2
Guilherme Nunes <guiicnunes@gmail.com>
2021-09-13 22:36:38 -0300

    FEAT: Carousel esboço

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,26 @@
+import React from "react";
+import Slider from "react-slick";
+import projectsData from "../data/projectsData";
+import CardProjects from "./CardProjects";
 import '../style/projects.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function Projects() {
+  const settings = {
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    centerPadding: "60px",
+    slidesToShow: 3,
+    speed: 500
+  };
+  
   return (
     <section className='projects'>
-      Olá projetos!
+      <Slider {...settings}>
+        { projectsData.map( (data) => <CardProjects dataCard={ data } /> ) }
+      </Slider>
     </section>
   );
 }
---
+++ src/data/projectsData.js
@@ -0,0 +1,52 @@
+const projectsData = [
+  {
+    "link": "htts://www.google.com/",
+    "gif": "https://media0.giphy.com/media/8ZVP2yAQKjOyQ/giphy.gif",
+    "alt": "Gif do projeto X",
+    "techs": [
+      "Javascript",
+      "React",
+      "Redux",
+      "Comunicacao"
+    ],
+    "text": "Mussum Ipsum, cacilds vidis litro abertis. Aenean aliquam molestie leo, vitae iaculis nisl. Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose. Viva Forevis aptent taciti sociosqu ad litora torquent. Cevadis im ampola pa arma uma pindureta."
+  },
+  {
+    "link": "htts://www.google.com/",
+    "gif": "https://media0.giphy.com/media/8ZVP2yAQKjOyQ/giphy.gif",
+    "alt": "Gif do projeto X",
+    "techs": [
+      "Javascript",
+      "React",
+      "Redux",
+      "Comunicacao"
+    ],
+    "text": "Mussum Ipsum, cacilds vidis litro abertis. Aenean aliquam molestie leo, vitae iaculis nisl. Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose. Viva Forevis aptent taciti sociosqu ad litora torquent. Cevadis im ampola pa arma uma pindureta."
+  },
+  {
+    "link": "htts://www.google.com/",
+    "gif": "https://media0.giphy.com/media/8ZVP2yAQKjOyQ/giphy.gif",
+    "alt": "Gif do projeto X",
+    "techs": [
+      "Javascript",
+      "React",
+      "Redux",
+      "Comunicacao"
+    ],
+    "text": "Mussum Ipsum, cacilds vidis litro abertis. Aenean aliquam molestie leo, vitae iaculis nisl. Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose. Viva Forevis aptent taciti sociosqu ad litora torquent. Cevadis im ampola pa arma uma pindureta."
+  },
+  {
+    "link": "htts://www.google.com/",
+    "gif": "https://media0.giphy.com/media/8ZVP2yAQKjOyQ/giphy.gif",
+    "alt": "Gif do projeto X",
+    "techs": [
+      "Javascript",
+      "React",
+      "Redux",
+      "Comunicacao"
+    ],
+    "text": "Mussum Ipsum, cacilds vidis litro abertis. Aenean aliquam molestie leo, vitae iaculis nisl. Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose. Viva Forevis aptent taciti sociosqu ad litora torquent. Cevadis im ampola pa arma uma pindureta."
+  }
+];
+
+export default projectsData;
